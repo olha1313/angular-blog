146a91f2e9403bfcdd3f0b8a5e5915137e0e0ec0
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-18 21:40:05 +0200

    Added list of posts

--- src/app/admin/dashboad-page/dashboad-page.component.ts
+++ src/app/admin/dashboad-page/dashboad-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostsService } from '../../shared/services/posts.service';
+import { Post } from '../../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboad-page',
   templateUrl: './dashboad-page.component.html',
   styleUrls: ['./dashboad-page.component.scss']
 })
-export class DashboadPageComponent implements OnInit {
+export class DashboadPageComponent implements OnInit, OnDestroy {
+  public posts: Post[] = [];
+  postsSubscription!: Subscription;
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.postsSubscription = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 
+  public ngOnDestroy(): void {
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe();
+    }
+  }
+
+  public remove(id?: string) {
+
+  }
 }
